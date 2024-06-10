cd15dcb27901fca1284291f3fe89c743238fd1f9
najmusyathir <alsyathir@gmail.com>
2024-06-10 14:19:51 +0800

    Modify: Update new routes to new pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -24,8 +26,26 @@ Route::middleware([
     // Don't spam on DefaultController, make the others in their specific controller;
     // To go to specific controller, press Ctrl+Click on Targetcontroller [exp: click on line 23 'DefaultController']
 
-    //Patients routes
 
+
+    //Patients routes
+    {
+        Route::get('/patient', [PatientController::class, 'index'])->middleware(['auth', 'verified'])->name('patient.index');
+        Route::get('/patient/new', function () {return view('patients.add');})->middleware(['auth', 'verified'])->name('patient.new');
+        Route::post('/patient/new/add', [PatientController::class, 'patientAdd'])->middleware(['auth', 'verified'])->name('patient.add');
+        Route::get('/patient/{id}', [PatientController::class, 'patientDetail'])->middleware(['auth', 'verified'])->name('patient.details');
+        Route::put('/patient/{id}/update', [PatientController::class, 'patientUpdate'])->middleware(['auth', 'verified'])->name('patient.update');
+        Route::delete('/patient/{id}/delete', [PatientController::class, 'patientDelete'])->middleware(['auth', 'verified'])->name('patient.delete');
+    }
 
     //Staff routes
+
+    {
+        Route::get('/staff', [StaffController::class, 'index'])->middleware(['auth', 'verified'])->name('staff.index');
+        Route::get('/staff/new', function () {return view('staffs.add');})->middleware(['auth', 'verified'])->name('staff.new');
+        Route::post('/staff/new/add', [StaffController::class, 'staffAdd'])->middleware(['auth', 'verified'])->name('staff.add');
+        Route::get('/staff/{id}', [StaffController::class, 'staffDetail'])->middleware(['auth', 'verified'])->name('staff.details');
+        Route::put('/staff/{id}/update', [StaffController::class, 'staffUpdate'])->middleware(['auth', 'verified'])->name('staff.update');
+        Route::delete('/staff/{id}/delete', [StaffController::class, 'staffDelete'])->middleware(['auth', 'verified'])->name('staff.delete');
+    }
 });
