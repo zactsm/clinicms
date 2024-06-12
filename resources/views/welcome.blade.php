a803eea8af1607d9eb7b4c963c003b1acc7467bf
zactsm <muazzamhazmiii@gmail.com>
2024-06-12 08:50:24 +0800

    Update: Include Make Payment tab to Welcome when signed in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,12 @@
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
                                 @auth
+                                <a
+                                href="{{ route('payment') }}"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                            >
+                                Make Payment
+                            </a>
                                     <a
                                         href="{{ url('/dashboard') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
